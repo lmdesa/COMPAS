de2db073ecb8550d2348b332c8677c0ec737d17f
Mike Lau <mike.lau@monash.edu>
2020-10-25 16:04:21 +1100

    (post-processing) add documentation and clean up functions in ClassCOMPAS

--- postProcessing/Folders/CosmicIntegration/PythonScripts/ClassCOMPAS.py
+++ postProcessing/Folders/CosmicIntegration/PythonScripts/ClassCOMPAS.py
@@ -4,8 +4,32 @@ import h5py as h5
 import os
 import totalMassEvolvedPerZ as MPZ
 
+class COMPASData(object):    
+    """Class for COMPAS output.
 
-class COMPASData(object):
+    Attributes:
+        path (str): Path to directory containing COMPAS h5 file.
+        fileName (str): Name of COMPAS h5 file.
+        lazyData (boo): Lazy data
+        Mlower (float): Minimum mass when drawing from Kroupa IMF.
+        Mupper (float): Maximum mass when drawing from Kroupa IMF.
+        binaryFraction (float): Binary fraction
+        metallicityGrid: Array of metallicities found in COMPAS output
+        metallicitySystems: Array of metallicities of each system in SystemParameters
+        delayTimes: Array of delay times (Myr) of each system in DoubleCompactObjects
+        mass1: Array of primary masses (Msun) of each system in DoubleCompactObjects
+        mass2: Array of secondary masses (Msun) of each system in DoubleCompactObjects
+        DCOmask: Mask for systems of interest in DoubleCompactObjects
+        allTypesMask: Mask for BBHs, BNSs, and BHNSs of interest in DoubleCompactObjects
+        BBHmask : Mask for BBHs of interest in DoubleCompactObjects 
+        DNSmask : Mask for DNSs of interest in DoubleCompactObjects 
+        BHNSmask : Mask for BHNSs of interest in DoubleCompactObjects 
+        mchirp: Array of chirp masses (Msun) of each system in DoubleCompactObjects
+        q: Array of mass ratios, q = m2/m1 in DoubleCompactObjects
+        pessimisticMask: Mask for systems in DoubleCompactObjects that are consistent with the pessimistic CE assumtion
+        massFormedAtEachZ: Stellar mass formed at each metallicity in the COMPAS output (in contrast with the actual stellar mass evolved by COMPAS)
+    """
+
     def __init__(
         self,
         path=None,
@@ -15,63 +39,84 @@ class COMPASData(object):
         Mupper=None,
         binaryFraction=None,
     ):
+        """Inits ClassCOMPAS
+
+        Args:
+            path (str, optional): Path to directory containing COMPAS h5 file. Defaults to None.
+            fileName (str, optional): Name of COMPAS h5 file. Defaults to "COMPAS_output.h5".
+            lazyData (bool, optional): Lazy data. Defaults to True.
+            Mlower (float, optional): Minimum mass when drawing from Kroupa IMF. Defaults to None.
+            Mupper (float, optional): Maximum mass when drawing from Kroupa IMF. Defaults to None.
+            binaryFraction (float, optional): Binary fraction. Defaults to None.
+
+        Raises:
+            ValueError: If COMPAS h5 file cannot be found from the specified path and fileName
+        """
         self.path = path
         self.fileName = fileName
         if self.path is None:
-            print("Just to double check you create instance of ClassCOMPAS without path/Data")
+            print("path not set in instance of ClassCOMPAS")
         elif not os.path.isfile(path + fileName):
-            raise ValueError(
-                "h5 file not found. Wrong path given? %s" % (path + fileName)
-            )
+            raise ValueError("h5 file not found in %s" % (path + fileName))
         elif os.path.isfile(path + fileName):
             pass
 
-        # Crucial values to be able to calculate MSSFR
         self.metallicityGrid = None
         self.metallicitySystems = None
-        self.delayTimes = None  # Myr
-        # Crucial values I need for selection effects
-        self.mass1 = None  # Msun
-        self.mass2 = None  # Msun
+        self.delayTimes = None
+        self.mass1 = None
+        self.mass2 = None
         self.DCOmask = None
         self.allTypesMask = None
         self.BBHmask = None
         self.DNSmask = None
         self.BHNSmask = None
 
-        # Additional arrays that might be nice to store
-        # to more quickly make some plots.
-        # If you need more memory might help a tiny bit to not do
         self.lazyData = lazyData
-        self.mChirp = None  # Msun
+        self.mChirp = None
         self.q = None
-        self.optimisticmask = None
+        self.pessimisticMask = None
 
-        # Needed to recover true solar mass evolved
-        self.Mlower = Mlower  # Msun
-        self.Mupper = Mupper  # Msun
+        self.Mlower = Mlower
+        self.Mupper = Mupper
         self.binaryFraction = binaryFraction
-        self.totalMassEvolvedPerZ = None  # Msun
+        self.massFormedAtEachZ = None
 
-        print("ClassCOMPAS: Remember to self.setGridAndMassEvolved()")
+        print("ClassCOMPAS: Remember to self.setZgridAndMassEvolved()")
         print("                   then  self.setCOMPASDCOmask()")
         print("                   then  self.setCOMPASData()")
 
     def setCOMPASDCOmask(
         self, types="BBH", withinHubbleTime=True, pessimistic=True, noRLOFafterCEE=True
     ):
-        # By default, we mask for BBHs that merge within a Hubble time, assumming
-        # the pessimistic CEE prescription (HG donors cannot survive a CEE) and
-        # not allowing immediate RLOF post-CEE
-        Data = h5.File(self.path + self.fileName)
-        fDCO = Data["DoubleCompactObjects"]
-        fCEE = Data["CommonEnvelopes"]
+        """Set masks for systems of interest in DoubleCompactObjects
+
+        Args:
+            types (str, optional): Stellar type to mask for ('BBH','BNS','BHNS','all'). Defaults to "BBH".
+            withinHubbleTime (bool, optional): True if masking for DCOs that merge within a Hubble time. Defaults to True.
+            pessimistic (bool, optional): True if using the pessimistic CE prescription (HG donors cannot survive a CEE). Defaults to True.
+            noRLOFafterCEE (bool, optional): True if not allowing immediate RLOF post-CE. Defaults to True.
+
+        Raises:
+            ValueError: If types is not one of 'BBH','BNS','BHNS', or 'all'
+        """    
+        data = h5.File(self.path + self.fileName)
+        fDCO = data["DoubleCompactObjects"]
+        fCEE = data["CommonEnvelopes"]
 
         # Masks for DCO type
-        maskBBH = (fDCO["Stellar_Type_1"][()] == 14) & (fDCO["Stellar_Type_2"][()] == 14)
-        maskDNS = (fDCO["Stellar_Type_1"][()] == 13) & (fDCO["Stellar_Type_2"][()] == 13)
-        maskBHNS = ((fDCO["Stellar_Type_1"][()] == 14) & (fDCO["Stellar_Type_2"][()] == 13)) \
-                    |((fDCO["Stellar_Type_1"][()] == 13) & (fDCO["Stellar_Type_2"][()] == 14))
+        maskBBH = (fDCO["Stellar_Type_1"][()] == 14) &\
+                  (fDCO["Stellar_Type_2"][()] == 14)
+        maskDNS = (fDCO["Stellar_Type_1"][()] == 13) &\
+                  (fDCO["Stellar_Type_2"][()] == 13)
+        maskBHNS = (
+                (fDCO["Stellar_Type_1"][()] == 14) &\
+                (fDCO["Stellar_Type_2"][()] == 13)
+            ) |\
+            (
+                (fDCO["Stellar_Type_1"][()] == 13) &\
+                (fDCO["Stellar_Type_2"][()] == 14)
+            )
         maskAllTypes = maskBBH | maskDNS | maskBHNS
 
         if types == "BBH":
@@ -85,7 +130,7 @@ class COMPASData(object):
         else:
             raise ValueError("type=%s not one of 'BBH', 'BNS', 'BHNS', 'all'" % (types))
 
-        # Mask DCOs merging within Hubble time
+        # Mask for DCOs merging within Hubble time
         if withinHubbleTime:
             maskHubble = fDCO["Merges_Hubble_Time"][()] == True
         else:
@@ -95,16 +140,15 @@ class COMPASData(object):
         CEEseeds = fCEE["SEED"][()]
         DCOseeds = fDCO["SEED"][()]
 
-        if (pessimistic or noRLOFafterCEE):
-            maskDCOCEEs = np.in1d(CEEseeds, DCOseeds) # Mask for CEEs involved in forming DCOs
-            DCOCEEseeds = CEEseeds[maskDCOCEEs] # Seeds of CEEs involved in forming DCO
+        if pessimistic or noRLOFafterCEE:
+            maskDCOCEEs = np.in1d(CEEseeds, DCOseeds)  # Mask for CEEs involved in forming DCOs
+            DCOCEEseeds = CEEseeds[maskDCOCEEs]  # Seeds of CEEs involved in forming DCOs
 
         # Mask for DCOs formed assumming pessimistic CEE
         if pessimistic:
             optimisticFlagForDCOCEEs = fCEE["Optimistic_CE"][()][maskDCOCEEs]
-            # Seeds of DCOs that have optimistic CEE
-            optimisticCEEseeds = np.unique(DCOCEEseeds[optimisticFlagForDCOCEEs])
-            maskOptimistic = np.in1d(DCOseeds,optimisticCEEseeds)
+            optimisticCEEseeds = np.unique(DCOCEEseeds[optimisticFlagForDCOCEEs]) # Seeds of DCOs that have optimistic CEE
+            maskOptimistic = np.in1d(DCOseeds, optimisticCEEseeds)
             maskPessimistic = np.logical_not(maskOptimistic)
         else:
             maskPessimistic = np.ones(len(fDCO["ID"][()]), dtype=bool)
@@ -114,7 +158,7 @@ class COMPASData(object):
             immediateRLOFflagforDCOCEEs = fCEE["Immediate_RLOF>CE"][()][maskDCOCEEs]
             # Seeds of DCOs that have immediate RLOF post-CEE
             immediateRLOFCEEseeds = np.unique(DCOCEEseeds[immediateRLOFflagforDCOCEEs])
-            maskRLOFafterCEE = np.in1d(DCOseeds,immediateRLOFCEEseeds)
+            maskRLOFafterCEE = np.in1d(DCOseeds, immediateRLOFCEEseeds)
             maskNoRLOFafterCEE = np.logical_not(maskRLOFafterCEE)
         else:
             maskNoRLOFafterCEE = np.ones(len(fDCO["ID"][()]), dtype=bool)
@@ -125,30 +169,26 @@ class COMPASData(object):
         self.DNSmask = maskDNS & maskHubble & maskPessimistic & maskNoRLOFafterCEE
         self.BHNSmask = maskBHNS & maskHubble & maskPessimistic & maskNoRLOFafterCEE
         self.allTypesMask = maskAllTypes & maskHubble & maskPessimistic & maskNoRLOFafterCEE
-        self.optimisticmask = maskPessimistic
-        Data.close()
+        self.pessimisticMask = maskPessimistic
+        data.close()
 
-    def setGridAndMassEvolved(self):
-        # The COMPAS simulation does not evolve all stars
-        # give me the correction factor for the total mass evolved
-        # I assume each metallicity has the same limits, and does correction
-        # factor, but the total mass evolved might be different.
-        # This does not change when we change types and other masks this is
-        # general to the entire simulation so calculate once
-        _, self.totalMassEvolvedPerZ = MPZ.totalMassEvolvedPerZ(
+    def setZgridAndMassEvolved(self):
+        """Sets metallicity grid from COMPAS output and calculates the total star-forming
+        mass at each metallicity
+        """      
+        _, self.massFormedAtEachZ = MPZ.getMassFormedAtEachZ(
             path=self.path,
             fileName=self.fileName,
             Mlower=self.Mlower,
             Mupper=self.Mupper,
             binaryFraction=self.binaryFraction,
         )
-        # Want to recover entire metallicity grid, assume that every metallicity
-        # evolved shows in all systems again should not change within same run
-        # so dont redo if we reset the data
-        Data = h5.File(self.path + self.fileName)
-        metallicities = Data["SystemParameters"]["Metallicity@ZAMS_1"][()]
-        self.metallicityGrid = np.unique(metallicities)
-        Data.close()
+
+        # Get metallicity grid
+        data = h5.File(self.path + self.fileName)
+        metallicityGrid = data["SystemParameters"]["Metallicity@ZAMS_1"][()]
+        self.metallicityGrid = np.unique(metallicityGrid)
+        data.close()
 
     def setCOMPASData(self):
         Data = h5.File(self.path + self.fileName)
@@ -185,13 +225,21 @@ class COMPASData(object):
 
         Data.close()
 
-    def recalculateTrueSolarMassEvolved(self, Mlower, Mupper, binaryFraction):
-        # Possibility to test assumptions of True solar mass evolved
+    def getMassFormedAtEachZ(self, Mlower, Mupper, binaryFraction):
+        """Set attributes related to mass sampling and calculates the total
+        star-forming mass at each metallicity corresponding to the mass drawn
+        and actually evolved by COMPAS.
+
+        Args:
+            Mlower ([float]): Minimum mass when drawing from Kroupa IMF.
+            Mupper ([float]): Maximum mass when drawing from Kroupa IMF.
+            binaryFraction ([float]): Binary fraction.
+        """
         self.Mlower = Mlower
         self.Mupper = Mupper
         self.binaryFraction = binaryFraction
-        _, self.totalMassEvolvedPerZ = MPZ.totalMassEvolvedPerZ(
-            pathCOMPASh5=self.path,
+        _, self.massFormedAtEachZ = MPZ.getMassFormedAtEachZ(
+            path=self.path,
             Mlower=self.Mlower,
             Mupper=self.Mupper,
             binaryFraction=self.binaryFraction,
